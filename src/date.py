dec3cd385a1da9cc317c7930de4e805e4ccdc3af
Stephen Jones <stephenj99642@gmail.com>
2026-06-02 20:06:25 +0100

    Turn a day into markdown

--- src/date.py
+++ src/date.py
@@ -1,6 +1,4 @@
-import datetime
-import os
-import sys
+import json
 
 import click
 from dateutil.parser import parse as dateutil_parse
@@ -15,8 +13,21 @@ def main(date: str):
     # The above creates a combined proper taking into account the temporal and
     # sanctoral cycle
     day = controller.get_proper_by_date(date_parsed, "en")
-    
 
+    # This should contain two propers, the english and the latin
+    assert len(day) == 1
+    assert len(day[0]) == 2
+    prop_en = day[0][0]
+    prop_ln = day[0][1]
+
+    # I've checked that, at least for 2026, this always matches the same IDs together
+    for part_en, part_ln in zip(prop_en.serialize(), prop_ln.serialize()):
+        assert part_en["id"] == part_ln["id"]
+
+        print(f"## {part_en['label']}")
+        print(f"{part_en['body']}")
+        print()
+    
 
 if __name__ == "__main__":
     main()
